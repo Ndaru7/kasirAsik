97072241e19df66bee8511f880eaa245360306dc
indra <muhammadindraardhana6@gmail.com>
2025-04-20 10:05:42 +0700

    Update view.py

--- backend/kasir/views.py
+++ backend/kasir/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Cart, Product, Category, Transaction, DetailTransaction
 from .serializers import (CartSerializers, ProductSerializers,
                           CategorySerializers, TransactionSerializers,
@@ -10,14 +10,14 @@ from .serializers import (CartSerializers, ProductSerializers,
 class ProductListView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class ProductRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializers
     lookup_url_kwarg = "product_id"
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 # ======================= #
 
 
@@ -25,14 +25,14 @@ class ProductRetrieveView(generics.RetrieveUpdateDestroyAPIView):
 class CategoryListView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializers
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class CategoryRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializers
     lookup_url_kwarg = "category_id"
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 # ======================== #
 
 
@@ -40,14 +40,14 @@ class CategoryRetrieveView(generics.RetrieveUpdateDestroyAPIView):
 class CartListView(generics.ListCreateAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartSerializers
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class CartRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CartSerializers
     lookup_url_kwarg = "cart_id"
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 # ==================== #
 
 
@@ -55,14 +55,14 @@ class CartRetrieveView(generics.RetrieveUpdateDestroyAPIView):
 class TransactionListView(generics.ListCreateAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializers
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class TransactionRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializers
     lookup_url_kwarg = "transaction_id"
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 # =========================== #
 
 
@@ -70,12 +70,12 @@ class TransactionRetrieveView(generics.RetrieveUpdateDestroyAPIView):
 class DetailTransactionListView(generics.ListCreateAPIView):
     queryset = DetailTransaction.objects.all()
     serializer_class = DetailTransactionSerializers
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class DetailTransactionRetrieveView(generics.RetrieveUpdateDestroyAPIView):
     queryset = DetailTransaction.objects.all()
     serializer_class = DetailTransactionSerializers
     lookup_url_kwarg = "detail_transaction_id"
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 # =================================== #
