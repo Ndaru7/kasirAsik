0862a32b9d5a7a78ca432e127fe48baa456b1ae5
Ndaru <daruprasetiyo123@gmail.com>
2025-04-20 23:18:22 +0700

    perf(url): major update for urls
    modify url biasa menjadi router untuk
    fleksibilitas dan mengurangi pengulangan
    penulisan kode

--- backend/kasir/urls.py
+++ backend/kasir/urls.py
@@ -1,16 +1,15 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet, CategoryViewSet, TransactionViewSet, DetailTransactionViewSet
+
+
+router = DefaultRouter()
+router.register(r"products", ProductViewSet)
+router.register(r"category", CategoryViewSet)
+router.register(r"transaction", TransactionViewSet)
+router.register(r"detail_transaction", DetailTransactionViewSet)
 
 
 urlpatterns = [
-    path("products/", views.ProductListView.as_view(), name="product-list"),
-    path("products/<int:product_id>", views.ProductRetrieveView.as_view(), name="product-retrieve"),
-    path("category/", views.CategoryListView.as_view(), name="category-list"),
-    path("category/<int:category_id>", views.CategoryRetrieveView.as_view(), name="category-retrieve"),
-    path("cart/", views.CartListView.as_view(), name="cart-list"),
-    path("cart/<int:cart_id>", views.CartRetrieveView.as_view(), name="cart-retrieve"),
-    path("transaction/", views.TransactionListView.as_view(), name="transaction-list"),
-    path("transaction/<int:transaction_id>", views.TransactionRetrieveView.as_view(), name="transaction-retrieve"),
-    path("detail_transaction/", views.DetailTransactionListView.as_view(), name="detail-transaction-list"),
-    path("detail_transaction/<int:detail_transaction_id>", views.DetailTransactionRetrieveView.as_view(), name="detail-transaction-retrieve"),
+    path("", include(router.urls))
 ]
