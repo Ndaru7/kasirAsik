b018a009f209762f7b9055ac97cb3a979f45d5be
Ndaru <daruprasetiyo123@gmail.com>
2025-04-20 22:02:57 +0700

    perf(serializer): major update for serializer
    modify sebagian besar pada semua class serializer

--- backend/kasir/serializers.py
+++ backend/kasir/serializers.py
@@ -2,37 +2,57 @@ from rest_framework import serializers
 from .models import Cart, Product, Category, Transaction, DetailTransaction
 
 
-class CategorySerializers(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ("id", "name")
 
 
-class ProductSerializers(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+    id_category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all(),
+        source="category",
+        write_only=True
+    )
 
     class Meta:
         model = Product
-        fields = ("id", "name", "price", "stock", "id_category")
+        fields = ("id", "name", "price", "stock", "category", "id_category")
 
 
-class CartSerializers(serializers.ModelSerializer):
-    # product = ProductSerializers(read_only=True)
+class CartSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+    id_product = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        source="product",
+        write_only=True
+    )
 
     class Meta:
         model = Cart
-        fields = ("id", "product", "qty")
+        fields = ("id", "product", "id_product", "qty", "total_price")
 
 
-class TransactionSerializers(serializers.ModelSerializer):
-    # cart = CartSerializers(read_only=True)
+class TransactionSerializer(serializers.ModelSerializer):
+    items = CartSerializer(many=True)
 
     class Meta:
         model = Transaction
-        fields = ("id", "date", "cart", "total_price")
+        fields = ("id", "date", "total", "items")
+
+    def create(self, validated_data):
+        items_data = validated_data.pop("items")
+        transaction = Transaction.objects.create(**validated_data)
+
+        for item in items_data:
+            Cart.objects.create(transaction=transaction, **item)
+
+        return transaction
 
 
-class DetailTransactionSerializers(serializers.ModelSerializer):
-    transaction = TransactionSerializers()
+class DetailTransactionSerializer(serializers.ModelSerializer):
+    transaction = CartSerializer(read_only=True)
 
     class Meta:
         model = DetailTransaction
