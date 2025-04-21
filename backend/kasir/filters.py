65b401e0c03dbdfa64adbacbbc15535072c29dae
faza <pratamafaza24@gmail.com>
2025-04-22 06:06:21 +0700

    create filters

---
+++ backend/kasir/filters.py
@@ -0,0 +1,8 @@
+from .models import Product, Transaction
+from django_filters import rest_framework as filters
+
+class TransactionFilter(filters.FilterSet):
+    date = filters.DateFilter(field_name="date", lookup_expr="date")
+    start = filters.DateFilter(field_name="start", lookup_expr="gte")
+    end = filters.DateFilter(field_name="end", lookup_expr="lte")
+    product_name = filters.CharFilter(field_name="items__product__name", lookup_expr="icontains")
