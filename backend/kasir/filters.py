c6c0a4e6583e419272ed366cca7f7854035bf1fc
faza <pratamafaza24@gmail.com>
2025-04-22 07:02:15 +0700

    add class meta

--- backend/kasir/filters.py
+++ backend/kasir/filters.py
@@ -6,3 +6,7 @@ class TransactionFilter(filters.FilterSet):
     start = filters.DateFilter(field_name="start", lookup_expr="gte")
     end = filters.DateFilter(field_name="end", lookup_expr="lte")
     product_name = filters.CharFilter(field_name="items__product__name", lookup_expr="icontains")
+
+    class Meta:
+        model = Transaction
+        fields = ("date", "start", "end", "product_name")
