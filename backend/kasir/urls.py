33edf75284e407ff4b92dddd157ee858f5387998
Ndaru <daruprasetiyo123@gmail.com>
2025-05-30 22:51:09 +0700

    feat: tambah urls untuk API profile

--- backend/kasir/urls.py
+++ backend/kasir/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (
     ProductViewSet, CategoryViewSet, TransactionViewSet,
-    PaymentViewSet,DetailTransactionViewSet
+    PaymentViewSet,DetailTransactionViewSet, ProfileViewSet
 )
 
 
@@ -12,6 +12,7 @@ router.register(r"category", CategoryViewSet)
 router.register(r"transaction", TransactionViewSet)
 router.register(r"payment", PaymentViewSet)
 router.register(r"detail_transaction", DetailTransactionViewSet)
+router.register(r"profile", ProfileViewSet)
 
 
 urlpatterns = [
