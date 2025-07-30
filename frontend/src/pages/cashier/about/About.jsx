3ccee553bc4db65ecd5cef9a77e8d7196f8486c6
Zerebyakov <zlfnaras@gmail.com>
2025-07-30 18:07:13 +0700

    Frontend : add display photo on cashier dashboard

--- frontend/src/pages/cashier/about/About.jsx
+++ frontend/src/pages/cashier/about/About.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import CashierLayout from '../layout/CashierLayout'
+import CircularGallery from '../components/CircularGallery'
 
 
 
 const About = () => {
   return (
-    <CashierLayout>About</CashierLayout>
+    <CashierLayout>
+      <div>
+        <p className='text-5xl font-bold mb-6 flex justify-center'>Our Teams</p>
+      </div>
+      <div style={{ height: '600px', position: 'relative' }}>
+        <CircularGallery bend={3} textColor="#000000" borderRadius={0.05} scrollEase={0.02} />
+      </div>
+    </CashierLayout>
   )
 }
 
